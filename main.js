2be7e697694e1af2c5f8a38a1bf5e1f2f420e374
Jacob <jakeboone@aol.com>
2014-06-23 16:18:30 -0600

    exercise completed

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 var streetname = prompt("Please type the name of your street.")
+alert("De-resolution on " + streetname + " " + "has now begun.")
 
-	alert("De-resolution on " + streetname + " " + "has now begun.")
 
-console.log(streetname.length)
+
+alert("There are " + streetname.length + " " + "characters in the word.")	
+
+
+
+alert("The third character is " + "'" + streetname[2] + "'" + ".")	
+
+
+alert("Lowercase: " + streetname.toLowerCase())	
+
+
+alert("Uppercase: " + streetname.toUpperCase())	
+
+
+alert("I used to live on " + streetname + " " + "in the summertime of 2014.")	
+
+alert("Subword: " + streetname.substring(1,4))
